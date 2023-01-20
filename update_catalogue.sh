7a7ddf21f5f724fd1782d5445f8d757e1b4ba754
amina-matt <[amina.matt@epfl.ch]>
2023-01-20 13:24:04 +0100

    test

--- update_catalogue.sh
+++ update_catalogue.sh
@@ -3,9 +3,10 @@
 # call the python script to update the folders with .md file for each book
 python update_books.py
 
-# push the new .md files in the github repository 
-token = "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"
-$token | glitch1312 | git push https://github.com/glitch1312/biblio-la-molene-catalogue 
-#Username: <my-username>
-#Password: <my-personal-access-token>
+# push the new .md files in the github repository
 
+token="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"
+git push https://github.com/glitch1312/biblio-la-molene-catalogue
+
+
+git push https://glitch1312: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@github.com/glitch1312/biblio-la-molene-catalogue --all
