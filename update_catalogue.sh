2a6a1961dee5fb19e933e8968c06826f86c72325
amina-matt <[amina.matt@epfl.ch]>
2023-01-20 10:25:03 +0100

    automated push test

--- update_catalogue.sh
+++ update_catalogue.sh
@@ -5,4 +5,7 @@ python update_books.py
 
 # push the new .md files in the github repository 
 token = "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"
+$token | glitch1312 | git push https://github.com/glitch1312/biblio-la-molene-catalogue 
+#Username: <my-username>
+#Password: <my-personal-access-token>
 
